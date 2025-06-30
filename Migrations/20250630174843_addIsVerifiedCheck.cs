e76463b0c2776543bfefffeb9ec501e40587abb8
MuhammadHuraira <muhammadhuraira4@gmail.com>
2025-06-30 22:50:50 +0500

    Add IsVerified check in user

---
+++ Migrations/20250630174843_addIsVerifiedCheck.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlomaCareAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class addIsVerifiedCheck : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IsVerified",
+                table: "users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsVerified",
+                table: "users");
+        }
+    }
+}
