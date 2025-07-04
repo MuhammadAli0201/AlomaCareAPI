5e6c3c0bb9828e9f9c60143ffefd225a0a699e08
MuhammadHuraira <muhammadhuraira4@gmail.com>
2025-07-04 23:56:19 +0500

    Set basic Help Rsources CRUD

---
+++ Models/HelpResource.cs
@@ -0,0 +1,11 @@
+﻿namespace AlomaCareAPI.Models
+{
+    public class HelpResource
+    {
+        public int Id { get; set; }
+        public string ResourceName { get; set; }
+        public double Size { get; set; }
+        public string Type { get; set; }
+        public string FilePath { get; set; }
+    }
+}
