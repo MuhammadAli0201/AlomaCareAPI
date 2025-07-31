264b699e15664f3fb63851bc3111527cd69ed723
MuhammadHuraira <muhammadhuraira4@gmail.com>
2025-07-31 23:15:37 +0500

    Add report DTOs

---
+++ AlomaCare.Models/DTOs/ReportDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlomaCare.Models.DTOs
+{
+    public class ReportDTO
+    {
+        public string ReportType { get; set; }
+        public DateTime ReportMonthAndYear { get; set; }
+        public List<ReportRecordDTO> ReportRecords { get; set; }
+        public List<ReportYearTotalsDTO> YearTotals { get; set; }
+    }
+}
---
+++ AlomaCare.Models/DTOs/ReportYearTotalsDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlomaCare.Models.DTOs
+{
+    public class ReportYearTotalsDTO
+    {
+        public string Category { get; set; }
+        public double Percentage { get; set; }
+    }
+}
