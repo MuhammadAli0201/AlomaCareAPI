ad5a474202f786e0ef82a63ff7d3a072bf865fab
MuhammadHuraira <muhammadhuraira4@gmail.com>
2025-08-07 23:38:08 +0500

    Add background service class

---
+++ AlomaCare.Api/Services/InternRotationEndNotificationService.cs
@@ -0,0 +1,24 @@
+﻿
+namespace AlomaCare.Api.Services
+{
+    public class InternRotationEndNotificationService : BackgroundService
+    {
+        private readonly IServiceScopeFactory scopeFactory;
+        private readonly ILogger<InternRotationEndNotificationService> logger;
+
+        public InternRotationEndNotificationService(IServiceScopeFactory scopeFactory,
+            ILogger<InternRotationEndNotificationService> logger)
+        {
+            this.scopeFactory = scopeFactory;
+            this.logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                // Logic to trigger emails to interns
+            }
+        }
+    }
+}
